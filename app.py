c47558c8236924ec86286aa1e27b88686457a7b6
Lavanya-lahari <pullalavanya3@gmail.com>
2025-02-23 14:03:45 +0530

    modified app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+def is_palindrome(word):
+    return word == word[::-1]
+
+# Input from user
+word = input("Enter a word: ")
+
+# Check if the word is a palindrome
+if is_palindrome(word):
+    print(f"{word} is a palindrome.")
+else:
+    print(f"{word} is not a palindrome.")
+
